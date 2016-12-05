f9a20614607479097b615d34a8b3c0ab9d3b28bd
joongbu <joongbu>
2016-12-05 17:48:34 +0900

    add drop_site and update function

--- main.cpp
+++ main.cpp
@@ -10,21 +10,12 @@
 #include <libnetfilter_queue/libnetfilter_queue.h>
 #include <glog/logging.h> //google log header
 #include <regex> //seach host
+#include <set>
+#include <thread>
 using namespace std;
-/* returns packet id */
-string getHostAddr(char*_data)
-{
-    string data(_data);
-    smatch result;
-    regex pattern("Host: .*");
-
-    if (regex_search(data, result, pattern)) {
-        return result[1];
-    }
-    cout<<"no search"<<endl;
-    return "";
-
-}
+//map<string,int> site;
+typedef set<string> d_type;
+d_type d_url;
 void glog(int option, string err)
 {
     google::InitGoogleLogging("netfilter");
@@ -34,77 +25,23 @@ void glog(int option, string err)
     VLOG(option) << "I'm printed when you run the program with --v=1 or higher";
     //VLOG(option) << "I'm printed when you run the program with --v=2 or higher";
 }
-
-static u_int32_t print_pkt (struct nfq_data *tb)
+void drop_url()
 {
-    int id = 0;
-    nfqnl_msg_packet_hdr *ph; //packet header
-    nfqnl_msg_packet_hw *hwph;
-    u_int32_t mark,ifi;
-    int ret;
-    u_char *data; //tcp packet data
-    ph = nfq_get_msg_packet_hdr(tb); //data parse function
-    if(ph)
+    string url;
+    cout<<"input url : ";
+    cin>>url;
+    d_type::iterator it = d_url.find(url);
+    if(it == d_url.end())
     {
-        id = ntohl(ph->packet_id);
-        printf("hw_protocol=0x%04x hook=%u id=%u ",
-               ntohs(ph->hw_protocol), ph->hook, id);
+    d_url.insert(url);
+    cout<<"success input"<<endl;
     }
-    hwph = nfq_get_packet_hw(tb);//print hw_addr
-    if (hwph)
-    {
-        int i, hlen = ntohs(hwph->hw_addrlen);
-        printf("hw_src_addr=");
-        for (i = 0; i < hlen-1; i++)
-            printf("%02x:", hwph->hw_addr[i]);
-        printf("%02x ", hwph->hw_addr[hlen-1]);
-    }
-    mark = nfq_get_nfmark(tb);
-    if (mark)
-        cout<<"mark="<<hex<<mark;
-    
-    ifi = nfq_get_indev(tb);
-    if (ifi)
-        cout<<"indev="<<hex<<ifi;
-    
-    ifi = nfq_get_outdev(tb);
-    if (ifi)
-        cout<<"outdev="<<hex<<ifi;
-    ifi = nfq_get_physindev(tb);
-    if (ifi)
-        cout<<"physindev="<<hex<<ifi;
-    
-    ifi = nfq_get_physoutdev(tb);
-    if (ifi)
-        cout<<"physoutdev="<<hex<<ifi;
-    
-    ret = nfq_get_payload(tb, &data);
-    if (ret >= 0)
-    {
-        cout<<"payload_len="<<ret<<endl;
-        for(int i = 0 ; i <= ret ; i++)
-            cout<<data[i];
-        cout<<endl;
-        //data is tcp data
 
-    }
-    fputc('\n', stdout);
-    
-    return id;
 }
 
-static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,struct nfq_data *nfa, void *data)
-{
-    u_int32_t id = print_pkt(nfa);
-    cout<<"entering callback\n";
-    return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
-}
-
-//this is callback function
 void dump(unsigned char*buf, size_t len)
 {
     size_t i;
-    
     for (i = 0; i < len; i++) {
         printf("%02x ", *buf++);
         if ((i + 1) % 16 == 0)
@@ -113,6 +50,62 @@ void dump(unsigned char*buf, size_t len)
     printf("\n");
     fflush(stdout);
 }
+
+/* returns packet id */
+string getHostAddr(struct nfq_data *tb)
+{
+    u_char *_data;
+    char *tcp_data;
+    tcp_data = (char *)malloc(1500);
+    nfq_get_payload(tb, &_data);
+    tcp_data  = (char *)(_data + 20 + 32);
+    string data(tcp_data);
+    smatch result;
+    regex pattern("Host: (.*)");
+    if (regex_search(data, result, pattern)) {
+        return result[1];
+    }
+        return "";
+}
+
+static u_int32_t print_pkt(struct nfq_data *tb)
+{
+    int id = 0;
+    nfqnl_msg_packet_hdr *ph; //packet header
+    int ret;
+    u_char *data; //tcp packet data
+    ph = nfq_get_msg_packet_hdr(tb); //data parse function
+    if(ph)
+    id = ntohl(ph->packet_id);
+    ret = nfq_get_payload(tb, &data);
+    if (ret >= 0)
+    {
+
+        string url;
+        cout<<"payload_len="<<dec<<ret<<endl;
+        //dump((unsigned char*)data,ret);
+        url = getHostAddr(tb);
+        if(!url.empty())
+        cout<<"site :"<<url<<endl;
+
+    }
+    fputc('\n', stdout);
+    return id;
+}
+
+static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,struct nfq_data *nfa, void *data)
+{
+    u_int32_t id = print_pkt(nfa);
+    d_type::iterator it = d_url.find(getHostAddr(nfa));
+    cout<<"entering callback\n";
+    if(it != d_url.end())
+    return nfq_set_verdict(qh, id, NF_DROP, 0 , NULL);  //packet Drop setting
+
+    return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL); //packet determind
+}
+
+//this is callback function
+
 void queue_setting()
 {
     struct nfq_handle *h;
@@ -121,13 +114,11 @@ void queue_setting()
     int rv;
     char buf[4096] __attribute__ ((aligned));
     cout<<"opening library handle\n";
-    
     h = nfq_open(); // return struct nfq_handle
     if (!h) {
         glog(1,"error during nfq_open()\n");
         exit(1);
     }
-    
     cout<<"unbinding existing nf_queue handler for AF_INET (if any)\n";
     if (nfq_unbind_pf(h, AF_INET) < 0) {
         glog(1,"error during nfq_unbind_pf()\n");
@@ -139,28 +130,23 @@ void queue_setting()
         glog(1,"error during nfq_bind_pf()\n");
         exit(1);
     }
-    
     cout<<"binding this socket to queue '0'\n";
     qh = nfq_create_queue(h,  0, &cb, NULL);
     if (!qh) {
         glog(1,"error during nfq_create_queue()\n");
         exit(1);
     }
-    
     cout<<"setting copy_packet mode\n";
     if (nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0) {
         glog(1,"can't set packet_copy mode\n");
         exit(1);
     }
-    
     fd = nfq_fd(h);
-    
     while(1)
     {
         if ((rv = recv(fd, buf, sizeof(buf), 0)) >= 0)
         {
             cout<<"pkt received\n"<<endl;
-            dump((unsigned char*)buf, rv); //print binary array
             nfq_handle_packet(h, buf, rv); //handle a packet received from the nfqueue subsystem
             continue;
         }
@@ -172,7 +158,6 @@ void queue_setting()
        * the doxygen documentation of this library on how to improve
        * this situation.
        */
-
         if (rv < 0 && errno == ENOBUFS)
         {
             cout<<"losing pacekt!!\n";
@@ -182,10 +167,8 @@ void queue_setting()
         glog(1,"recv failed\n");
         break;
     }
-    
     cout<<"unbinding from queue 0\n";
     nfq_destroy_queue(qh);
-    
     cout<<"closing library handle\n";
     nfq_close(h);
     
@@ -194,5 +177,6 @@ void queue_setting()
 
 int main(int argc, char **argv)
 {
+    //drop_url();
     queue_setting();
 }
